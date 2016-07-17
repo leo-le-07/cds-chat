9d62655548cfd34ab45a884cf150205498597714
Leo Le <coder.leo.le@gmail.com>
2016-07-18 05:42:15 +0700

    Adding filepicker-rails config

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'faker'
 gem 'kaminari'
 gem 'jquery-infinite-pages'
 gem 'omniauth-facebook', '~> 3.0'
+gem 'filepicker-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module CdsChat
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.filepicker_rails.api_key = ENV['FILEPICKER_API_KEY']
   end
 end
