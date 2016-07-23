45f6c0745a40283279005e86bc4bc1bc8dfdc6db
Leo Le <coder.leo.le@gmail.com>
2016-07-23 22:03:34 +0700

    Add Allowed Request Origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://calm-tundra-22594.herokuapp.com', 'http://calm-tundra-22594.herokuapp.com']
 end
